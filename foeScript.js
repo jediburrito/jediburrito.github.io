02b7ab3aa1f5a536d534286584d72df50cd5263e
jediburrito <jediburrito@gmail.com>
2019-03-13 05:14:44 -0400

    Update foeScript.js

--- foeScript.js
+++ foeScript.js
@@ -199,9 +199,13 @@ function traverse(newBoard, player, depth, scoreTemp, newMinMaxxer, total) {
 		newBoard[scoreTemp[i].row][scoreTemp[i].column] = player;
 		scoreTemp[i].row--;
 		if(player === aiPlayer && scoreTemp[i].value >= 1000) {
+			if(depth === 0)
+				bestIndex = scoreTemp[i].column;
 			return (2000 - (depth * 10));
 		}
 		else if(player === huPlayer && scoreTemp[i].value <= -1000) {
+			if(depth === 0)
+				bestIndex = scoreTemp[i];
 			return (-2000 + (depth * 10));
 		}
 		
